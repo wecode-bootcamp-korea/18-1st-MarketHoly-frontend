c44373896e3b2f565ecadb954045b86de51302a9
chanho-Yoon <chyoon0512@gmail.com>
2021-03-15 18:10:20 +0900

    CRA 초기세팅 완료

---
+++ marketholy/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
+import './index.css';
+import './styles/common.scss';
+
+ReactDOM.render(<Routes/>, document.getElementById('root')
+);
