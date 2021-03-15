505dcd440dbef5f8ecfe0232f467dd98e5d3df81
chanho-Yoon <chyoon0512@gmail.com>
2021-03-15 19:58:47 +0900

    CRA 초기세팅 완료5

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/main';
+import Main from './Pages/Main/Main';
 
 class Routes extends React.Component {
   render() {
