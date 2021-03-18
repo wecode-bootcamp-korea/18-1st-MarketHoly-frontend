9fbaf8bdfdcdb7068feb8995d4566e2ce2527987
이대희 <ldaehi0205@gmail.com>
2021-03-19 02:10:46 +0900

    add: mypage 프로필 레이아웃 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/Main';
+// import Main from './Pages/Main/Main';
+import Mypageorderlist from './Pages/Mypage/Mypageorderlist';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Mypageorderlist} />
         </Switch>
       </Router>
     );
