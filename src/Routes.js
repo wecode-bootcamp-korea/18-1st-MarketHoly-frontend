d6222cf2554916882ac7367626708a21166a1919
SongDye1 <sodah6@gmail.com>
2021-03-21 14:44:11 +0900

    footer 레이아웃 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
 import Main from './Pages/Main/Main';
 
-class Routes extends React.Component {
+class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/Footer" component={Footer} />
         </Switch>
       </Router>
     );
