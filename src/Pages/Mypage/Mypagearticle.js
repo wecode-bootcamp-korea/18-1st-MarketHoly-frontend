e944c0afbf6d1bfa8dda64e3c3c4c54ec3866732
이대희 <ldaehi0205@gmail.com>
2021-03-22 15:02:28 +0900

    Modify: file Change error 5000 해결

---
+++ src/Pages/Mypage/Mypagearticle.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Select from 'react-select';
+import { AiOutlineRight } from 'react-icons/ai';
+import '../../styles/common.scss';
+import './Mypagearticle.scss';
+import Myiteminfo from './Myiteminfo';
+import Myalwayitem from './Myalwayitem';
+
+const options = [
+  { value: '전체기간', label: '전체기간' },
+  { value: '2021년', label: '2021년' },
+  { value: '2020년', label: '2020년' },
+  { value: '2019년', label: '2019년' },
+];
+
+const MyComponent = () => <Select options={options} />;
+
+export class Mypagearticle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      item: [],
+      wishItem: [],
+      color: ['', true, false, false],
+      color1: '1',
+    };
+  }
+
+  componentDidMount() {
+    fetch('/data/productinfo1.json')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          item: res,
+        });
+      });
+
+    fetch('/data/wishdata.json')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          wishItem: res,
+        });
+      });
+  }
+
+  removeWhish = () => {
+    this.setState({
+      wishItem: [],
+    });
+  };
+
+  removeWhishitem = e => {
+    console.log(e.target.value);
+    this.setState({
+      wishItem: this.state.wishItem.filter(item => e.target.value != item.id),
+    });
+  };
+
+  changecolor = e => {
+    let i;
+    console.log(this.state.color[e.target.value]);
+    this.setState({
+      color1: '',
+    });
+    for (i = 1; i < this.state.color.length; i++) {
+      e.target.value != i
+        ? (this.state.color[i] = false)
+        : (this.state.color[e.target.value] = !this.state.color[
+            e.target.value
+          ]);
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <article className="myArticle">
+          <div className="article">
+            <nav className="navBar">
+              <div className="myHoly">
+                <a>마이홀리</a>
+              </div>
+              <ul>
+                <Link to="/">
+                  <li
+                    className={this.state.color[1] ? 'clickChange' : false}
+                    onClick={this.changecolor}
+                    value="1"
+                  >
+                    <div>주문 내역</div>
+                    <div>
+                      <AiOutlineRight />
+                    </div>
+                  </li>
+                </Link>
+                <Link to="/Myalwayitem">
+                  <li
+                    className={this.state.color[2] ? 'clickChange' : false}
+                    onClick={this.changecolor}
+                    value="2"
+                  >
+                    <div>늘 사는 것</div>
+                    <div>
+                      <AiOutlineRight />
+                    </div>
+                  </li>
+                </Link>
+                <li>
+                  <div
+                    className={this.state.color[3] ? 'reclickChanged' : false}
+                    onClick={this.changecolor}
+                    value="3"
+                  >
+                    배송지 관리
+                  </div>
+                  <div>
+                    <AiOutlineRight />
+                  </div>
+                </li>
+                <li>
+                  <div>상품 후기</div>
+                  <AiOutlineRight />
+                </li>
+                <li>
+                  <div> 적립금</div>
+                  <AiOutlineRight />
+                </li>
+                <li>
+                  <div>쿠폰</div>
+                  <AiOutlineRight />
+                </li>
+                <li>
+                  <div>개인 정보 수정</div>
+                  <AiOutlineRight />
+                </li>
+              </ul>
+            </nav>
+            <section className="pageSection">
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={() => <Myiteminfo item={this.state.item} />}
+                />
+                <Route
+                  exact
+                  path="/Myalwayitem"
+                  render={() => (
+                    <Myalwayitem
+                      wishItem={this.state.wishItem}
+                      removeWhish={this.removeWhish}
+                      removeWhishitem={this.removeWhishitem}
+                    />
+                  )}
+                />
+              </Switch>
+            </section>
+          </div>
+        </article>
+      </Router>
+    );
+  }
+}
+
+export default Mypagearticle;
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/Main';
+import Mypageorderlist from './Pages/Mypage/Mypageorderlist';
+import Mypagearticle from './Pages/Mypage/Mypagearticle';
+import Mypage from './Pages/Mypage/Mypage';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          {/* <Route exact path="/" component={Mypageorderlist} /> */}
+          {/* <Route exact path="/" component={Mypagearticle} /> */}
+          <Route exact path="/" component={Mypage} />
         </Switch>
       </Router>
     );
