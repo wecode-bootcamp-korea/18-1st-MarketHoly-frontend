ecacb07308603929676ca698928f04a55dd21f79
SongDye1 <sodah6@gmail.com>
2021-03-23 20:11:50 +0900

    로그인 API 통신 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -25,6 +25,7 @@ class Login extends Component {
     if (idCheck.test(this.state.email) && this.state.password.length >= 10) {
       let summonerUrl = `user/login`;
       fetch(summonerUrl, {
+        //fetch url -> config.json으로 분리 필요
         method: 'POST',
         body: JSON.stringify({
           email: this.state.email,
@@ -32,11 +33,12 @@ class Login extends Component {
         }),
       })
         .then(res => res.json())
-        .then(res => {
-          console.log(res);
-          if (parseInt(res.status) === 200) {
+        .then(result => {
+          console.log(result);
+          if (result.message === 'SUCCESS') {
+            localStorage.setItem('token', result.access_token);
             alert('로그인 완료');
-            this.props.history.push('../Main');
+            this.props.history.push('/');
           } else {
             alert('로그인 실패');
           }
@@ -60,7 +62,7 @@ class Login extends Component {
                 <input className="securityConnect" type="checkbox" />
                 보안접속
               </label>
-              <div className="findLogin">
+              {/* <div className="findLogin">
                 <a href="/#" className="findId">
                   아이디 찾기
                 </a>
@@ -68,7 +70,7 @@ class Login extends Component {
                 <a href="/#" className="findPwd">
                   비밀번호 찾기
                 </a>
-              </div>
+              </div> */}
             </div>
             <button type="submit" className="loginBtn" onClick={this.loginSubmit}>
               로그인
