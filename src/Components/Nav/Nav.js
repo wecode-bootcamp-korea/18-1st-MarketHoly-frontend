4027d18b59c38d92231f9709c4497bd48e338d77
chanho-Yoon <chyoon0512@gmail.com>
2021-03-26 03:16:53 +0900

    진심 최종 프론트엔드 최종! navbar 반응형에서 고정형으로 .scss 수정

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -145,138 +145,140 @@ class Nav extends React.Component {
   render() {
     const { allCategoriesHover, hoverWidth, displayShowIndex, navbarList, toggleSearchInput, isNavFixed, userInfo, isLoginHover } = this.state;
     return (
-      <div className={'navbar ' + (isNavFixed && 'navbarFixed')} onMouseHover={this.allCategoriesHoverLeave} onScroll={this.onScrollGet}>
-        <div className="user-menu">
-          <NavBanner />
-          {Object.keys(userInfo).length === 0 ? (
-            <div className="user-list-menu">
-              <div className="list-join menu-list">
-                <Link to="/Signup" className="join-link">
-                  회원가입
-                </Link>
+      <div className={'navbarContainer'}>
+        <div className={'navbar ' + (isNavFixed && 'navbarFixed')} onMouseHover={this.allCategoriesHoverLeave} onScroll={this.onScrollGet}>
+          <div className="user-menu">
+            <NavBanner />
+            {Object.keys(userInfo).length === 0 ? (
+              <div className="user-list-menu">
+                <div className="list-join menu-list">
+                  <Link to="/Signup" className="join-link">
+                    회원가입
+                  </Link>
+                </div>
+                <div className="list-login menu-list">
+                  <Link to="/Login" className="login-link">
+                    &nbsp;로그인
+                  </Link>
+                </div>
               </div>
-              <div className="list-login menu-list">
-                <Link to="/Login" className="login-link">
-                  &nbsp;로그인
+            ) : (
+              <li className="userLoginMenu" onMouseEnter={this.loginMenuHover} onMouseLeave={this.loginMenuHover}>
+                <Link to="/mypage" className="use">
+                  <span className="ico_grade grade6">웰컴</span>
+                  <span className="txt">
+                    <span className="name">{userInfo.name}</span>
+                    <span className="sir">님</span>
+                    <GoTriangleDown />
+                  </span>
                 </Link>
-              </div>
-            </div>
-          ) : (
-            <li className="userLoginMenu" onMouseEnter={this.loginMenuHover} onMouseLeave={this.loginMenuHover}>
-              <Link to="/mypage" className="use">
-                <span className="ico_grade grade6">웰컴</span>
-                <span className="txt">
-                  <span className="name">{userInfo.name}</span>
-                  <span className="sir">님</span>
-                  <GoTriangleDown />
-                </span>
-              </Link>
-              <ul className="sub" style={{ display: isLoginHover ? 'flex' : 'none' }}>
-                <li>
-                  <Link to="">주문 내역</Link>
+                <ul className="sub" style={{ display: isLoginHover ? 'flex' : 'none' }}>
+                  <li>
+                    <Link to="">주문 내역</Link>
+                  </li>
+                  <li>
+                    <Link to="">배송지 관리</Link>
+                  </li>
+                  <li>
+                    <Link to="">늘 사는 것</Link>
+                  </li>
+                  <li>
+                    <Link to="">상품 후기</Link>
+                  </li>
+                  <li>
+                    <Link to="">적립금</Link>
+                  </li>
+                  <li>
+                    <Link to="">쿠폰</Link>
+                  </li>
+                  <li>
+                    <Link to="">개인 정보 수정</Link>
+                  </li>
+                  <li>
+                    <Link to="/" onClick={this.handleLogout}>
+                      로그아웃
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            )}
+          </div>
+          <div className="navbar-logo">
+            <Link to="/" className="logo-link">
+              <img src="/images/navlogo.png" alt="마켓홀리 로고" />
+            </Link>
+          </div>
+          <div className="navbar-menu">
+            <div className="menu-main">
+              <ul className="menu-main-list">
+                <li className="menu1" onMouseEnter={this.allCategoriesHoverEnter}>
+                  <Link to="#" className="menu1-link">
+                    <FiMenu className="menu1-icon" />
+                    <span className="menu-text">전체 카테고리</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link to="">배송지 관리</Link>
+                <li className="menu2">
+                  <Link
+                    to={{
+                      pathname: `/product`,
+                      state: {
+                        checkMenu: 'new',
+                      },
+                    }}
+                  >
+                    <span className="menu-text">신상품</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link to="">늘 사는 것</Link>
+                <li className="menu3">
+                  <Link
+                    to={{
+                      pathname: `/product`,
+                      state: {
+                        checkMenu: 'best',
+                      },
+                    }}
+                  >
+                    <span className="menu-text">베스트</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link to="">상품 후기</Link>
+                <li className="menu4">
+                  <Link to="#">
+                    <span className="menu-text">알뜰쇼핑</span>
+                  </Link>
                 </li>
-                <li>
-                  <Link to="">적립금</Link>
-                </li>
-                <li>
-                  <Link to="">쿠폰</Link>
-                </li>
-                <li>
-                  <Link to="">개인 정보 수정</Link>
-                </li>
-                <li>
-                  <Link to="/" onClick={this.handleLogout}>
-                    로그아웃
+                <li className="menu5">
+                  <Link to="#">
+                    <span className="menu-text">금주혜택</span>
                   </Link>
                 </li>
               </ul>
-            </li>
-          )}
-        </div>
-        <div className="navbar-logo">
-          <Link to="/" className="logo-link">
-            <img src="/images/navlogo.png" alt="마켓홀리 로고" />
-          </Link>
-        </div>
-        <div className="navbar-menu">
-          <div className="menu-main">
-            <ul className="menu-main-list">
-              <li className="menu1" onMouseEnter={this.allCategoriesHoverEnter}>
-                <Link to="#" className="menu1-link">
-                  <FiMenu className="menu1-icon" />
-                  <span className="menu-text">전체 카테고리</span>
-                </Link>
-              </li>
-              <li className="menu2">
-                <Link
-                  to={{
-                    pathname: `/product`,
-                    state: {
-                      checkMenu: 'new',
-                    },
-                  }}
-                >
-                  <span className="menu-text">신상품</span>
-                </Link>
-              </li>
-              <li className="menu3">
-                <Link
-                  to={{
-                    pathname: `/product`,
-                    state: {
-                      checkMenu: 'best',
-                    },
-                  }}
-                >
-                  <span className="menu-text">베스트</span>
-                </Link>
-              </li>
-              <li className="menu4">
-                <Link to="#">
-                  <span className="menu-text">알뜰쇼핑</span>
-                </Link>
-              </li>
-              <li className="menu5">
-                <Link to="#">
-                  <span className="menu-text">금주혜택</span>
-                </Link>
-              </li>
-            </ul>
-            <div className="side-search">
-              <form className="side-search-box" onSubmit={this.handleSubmitSearch}>
-                <input type="text" className="search-input" name="search" onChange={this.handleChange} ref={this.searchInputRef} />
-                {toggleSearchInput && (
-                  <button className="search-btn-cancel" onClick={this.resetInputValue}>
-                    <MdCancel className="search-btn-icon" />
+              <div className="side-search">
+                <form className="side-search-box" onSubmit={this.handleSubmitSearch}>
+                  <input type="text" className="search-input" name="search" onChange={this.handleChange} ref={this.searchInputRef} />
+                  {toggleSearchInput && (
+                    <button className="search-btn-cancel" onClick={this.resetInputValue}>
+                      <MdCancel className="search-btn-icon" />
+                    </button>
+                  )}
+                  <button className="search-btn" type="submit">
+                    <CgSearch className="search-btn-icon" />
                   </button>
-                )}
-                <button className="search-btn" type="submit">
-                  <CgSearch className="search-btn-icon" />
-                </button>
-              </form>
-            </div>
-            <div className="cart-count">
-              <div className="cart-count-link">
-                <FiShoppingCart className="cart-count-icon" />
+                </form>
+              </div>
+              <div className="cart-count">
+                <div className="cart-count-link">
+                  <FiShoppingCart className="cart-count-icon" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="menu-hover-box" style={allCategoriesHover ? { display: 'flex', width: `${hoverWidth}px` } : { display: 'none' }} onMouseLeave={this.allCategoriesHoverLeave}>
-            <div className="menu-hover">
-              <ul className="menu-hover-list">
-                {navbarList.map((list, index) => (
-                  <NavBarHoverList displayShowIndex={displayShowIndex} list={list} index={index + 1} key={index} categoriesMenuHoverEnter={this.categoriesMenuHoverEnter} />
-                ))}
-              </ul>
+            <div className="menu-hover-box" style={allCategoriesHover ? { display: 'flex', width: `${hoverWidth}px` } : { display: 'none' }} onMouseLeave={this.allCategoriesHoverLeave}>
+              <div className="menu-hover">
+                <ul className="menu-hover-list">
+                  {navbarList.map((list, index) => (
+                    <NavBarHoverList displayShowIndex={displayShowIndex} list={list} index={index + 1} key={index} categoriesMenuHoverEnter={this.categoriesMenuHoverEnter} />
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
